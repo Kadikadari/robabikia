258530326ad348253c79ae64d25d859fe15029ea
Kadikadari <kadikadari133@gmail.com>
2026-04-29 16:59:43 +0200

    اكتب هنا ماذا فعلت في هذا التعديل

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // إعدادات Firebase الحقيقية لمشروع روبابيكيا
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore, collection, addDoc, query, orderBy, onSnapshot, where, serverTimestamp, doc, updateDoc, arrayUnion, arrayRemove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore, collection, addDoc, query, orderBy, onSnapshot, where, serverTimestamp, doc, updateDoc, arrayUnion, arrayRemove, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBEv3Du_D1vnEhMMFaKyDS5TEMKqJYVML8",
@@ -22,5 +22,5 @@ export {
     auth, db, onAuthStateChanged, signInWithEmailAndPassword,
     createUserWithEmailAndPassword, signOut, collection,
     addDoc, query, orderBy, onSnapshot, where,
-    serverTimestamp, doc, updateDoc, arrayUnion, arrayRemove
+    serverTimestamp, doc, updateDoc, arrayUnion, arrayRemove, getDoc
 };
